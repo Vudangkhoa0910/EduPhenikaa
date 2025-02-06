8a0ab83ff8f5344aa53baba2d938770198427428
VuDangKhoa <dangkhoa09102004@gmail.com>
2025-02-06 12:13:53 +0700

    update teacher_new_tuan

--- backend/models/enrollment.model.js
+++ backend/models/enrollment.model.js
@@ -1,9 +1,9 @@
-// models/enrollment.model.js
+// enrollment.model.js
 const mongoose = require("mongoose");
 
 const enrollmentSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }, // **SỬA ref: "User" THÀNH ref: "user" (chữ thường)**
     courseId: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
     price: { type: Number, required: true },
     paymentMethod: { type: String, required: true },
